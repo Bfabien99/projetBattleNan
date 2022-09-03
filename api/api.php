2b2b127642075cf8968fd0dd61615fd3a40f72f3
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-03 13:39:59 +0000

    refactoring api

--- api/api.php
+++ api/api.php
@@ -1,17 +1,17 @@
 <?php
-// define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
+define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
 
 function getConnexion()
 {
     return new PDO("mysql:host=localhost;dbname=clando;charset=utf8","root","");
 }
 
-// function sendJSON($infos)
-// {   
-//     header("Access-Control-Allow-Origin: *");
-//     header("Content-type: application/json");
-//     echo json_encode($infos,JSON_UNESCAPED_UNICODE);
-// }
+function sendJSON($infos)
+{   
+    header("Access-Control-Allow-Origin: *");
+    header("Content-type: application/json");
+    echo json_encode($infos,JSON_UNESCAPED_UNICODE);
+}
 
 function getAllUsers()
 {
@@ -21,42 +21,62 @@ function getAllUsers()
     $stmt = $pdo->prepare($req);
     $stmt->execute();
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $users;
+    return sendJson($users);
 }
 
-function insertUser($nom,$prenoms,$contact,$date_naissance,$email,$pseudo,$pass){
+function insertUser($nom,$prenoms,$contact,$date_naissance,$email,$pseudo,$pass)
+{
     $pdo = getConnexion();
-    $query = $pdo->prepare("INSERT INTO users (nom,prenoms,contact,date_naissance,email,pseudo,pass) VALUES ('
-    :nom','
-    :prenoms','
-    :contact','
-    :date_naissance','
-    :email','
-    :pseudo','
-    :pass'");
-            $insert = $query->execute([
-                ':nom' => $nom,
-                ':prenoms' => $prenoms,
-                ':contact' => $contact,
-                ':date_naissance' => $date_naissance,
-                ':email' => $email,
-                ':pseudo' => $pseudo,
-                ':pass' => $pass,
-            ]);
-            if($insert){
-                return true;
-            }
-            else {
-                return false;
-            }
+    $req = "INSERT INTO users(nom,prenoms,contact,date_naissance,email,pseudo,password) VALUES(
+        :nom,:prenoms,:contact,:date_naissance,:email,:pseudo,:password)";
+    $query = $pdo->prepare($req);
+    $insert = $query->execute([
+        'nom' => $nom,
+        'prenoms' => $prenoms,
+        'contact' => $contact,
+        'date_naissance' => $date_naissance,
+        'email' => $email,
+        'pseudo' => $pseudo,
+        'password' => $pass
+    ]);
+    if ($insert) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function userLogin($email, $pass){
+    $pdo = getConnexion();
+    $req = "SELECT * FROM users WHERE email = '$email' AND password = '$pass'";
+
+    $stmt = $pdo->prepare($req);
+    $stmt->execute();
+    $users = $stmt->fetch(PDO::FETCH_ASSOC);
+    return sendJson($users);
 }
 
 //Table finance
-function postFinance(){
-
+function postFinance($userId,$domaine,$description,$somme)
+{
+    $pdo = getConnexion();
+    $req = "INSERT INTO finance(userId,domaine,description,somme) VALUES(
+        :userId,:domaine,:description,:somme)";
+    $query = $pdo->prepare($req);
+    $insert = $query->execute([
+        'userId' => $userId,
+        'domaine' => $domaine,
+        'description' => $description,
+        'somme' => $somme,
+    ]);
+    if ($insert) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //Table job
-function postJob(){
-
+function postJob()
+{
 }
