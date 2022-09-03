8dee6b67367889cea27e1f2cab911777e3194fb9
Onesiphore-chris <chrisleconquerant01@gmail.com>
2022-09-03 11:57:21 +0000

    creation du header et footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,46 @@
-</main>    
+</main> 
+
+<div class="footer">
+    <div class="box-container">
+
+    <div class="box">
+        <h3>Uink links</h3>
+        <a href="index.php"> <i class="fas fa-angle-right"></i>home</a>
+        <a href="compagnie.php"><i class="fas fa-angle-right"></i>compagnie us</a>
+        <a href="services.php"><i class="fas fa-angle-right"></i>services </a>
+        <a href="contact.php"><i class="fas fa-angle-right"></i>contact</a>
+        
+    </div> 
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>terns of user</a>
+        
+    </div> 
+
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +225-076-362-851</a>
+        <a href="#"> <i class="fas fa-phone"></i> +225-076-362-851</a>
+        <a href="#"> <i class="fas fa-envelope"></i>chris01@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> Abidjan, Côte d'ivoire- 225</a>
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook"></i> facebook</a>
+        <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+        <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+        <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+    </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Php Family</span> | all right reserved</div>
+</div>
+
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -5,20 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 </head>
 <body>
     <header>
-        <nav>
-            <ul>
-                <li><a href="">Accueil</a></li>
-                <li><a href="">A propos</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Connexion</a></li>
-            </ul>
-        </nav>
-        <div class="subheader">
+    <section class="header">
+            <a href="" class="logo"><span>ANTI-</span>MIGRATION</a>
+            <nav>
+                <div class="navbar">
+                    <a href="">acceuil</a>
+                    <a href="">compagnie</a>
+                    <a href="">services</a>
+                    <a href="">contact</a>
+                </div>
+            </nav>
+            <div><a href=""class="fas fa-user" id="btn-conexion">conexion</a></div>
+    </header>
+    <div class="sub-header">
+            <div class="header-title">
+                <h2>BIENVENUE DANS NOS LOCAUX</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br> Architecto, nobis.</p>
+                <a href="" class="btn1">Connsulter nos Centres<span class="fas fa-chevron-right"></span></a>
+            </div>
+            <div class="header-image">
+                <img src="images/campaign-creators-gMsnXqILjp4-unsplash.jpg" alt="">
+            </div>
+            </div>
             
         </div>
-    </header>
     <main>
