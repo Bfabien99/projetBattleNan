5c2df777b848a1ade35d357e006b874a29e83e0e
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-03 15:08:41 +0000

    try to merge

--- includes/insert.php
+++ includes/insert.php
@@ -2,13 +2,13 @@
 include('../api/api.php');
 if(isset($_POST['insert'])){
     if(!empty($_POST['nom']) && !empty($_POST['prenoms']) && !empty($_POST['contact']) && !empty($_POST['date_naissance']) && !empty($_POST['email']) && !empty($_POST['pseudo']) && !empty($_POST['password'])){
-        $nom = escapeString($_POST['nom']);
-        $prenoms = escapeString($_POST['prenoms']);
-        $contact =escapeString($_POST['contact']);
-        $date_naissance =escapeString($_POST['date_naissance']);
-        $email =escapeString($_POST['email']);
-        $pseudo =escapeString($_POST['pseudo']);
-        $password = escapeString($_POST['password']);
+        $nom = str_replace(' ','_',$_POST['nom']);
+        $prenoms = str_replace(' ','_',$_POST['prenoms']);
+        $contact =str_replace(' ','_',$_POST['contact']);
+        $date_naissance =str_replace(' ','_',$_POST['date_naissance']);
+        $email =str_replace(' ','_',$_POST['email']);
+        $pseudo =str_replace(' ','_',$_POST['pseudo']);
+        $password = str_replace(' ','_',$_POST['password']);
         $insert = file_get_contents("http://localhost/projetBattleNan/api/insert/$nom/$prenoms/$contact/$date_naissance/$email/$pseudo/$password");
         if($insert){
             echo "donné enregistré";
